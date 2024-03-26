f85020e0db0ef808a85ac8ed9729b9a03fd405d6
chaitu1 <chaitu143@gmail.com>
2024-03-26 19:54:44 +0530

    practing provisioner part-2

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -1,7 +1,9 @@
-resource "null_resource" "name" {
+resource "null_resource" "name1" {
   provisioner "local-exec" {
     command = "echo this is ${var.modname}"
   }
 }
-
+output "names" {
+  value = var.modname
+}
 variable "modname" {}
