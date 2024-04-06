0a69473fce969fc1f51f9e469e3d8c41215a532a
chaitu1 <chaitu143@gmail.com>
2024-04-06 13:08:17 +0530

    line in fiel

--- main.tf
+++ main.tf
@@ -49,17 +49,17 @@ provider "vault" {
 #  env_m                       = var.env
 #}
 #
-#module "backend" {
-#  depends_on = [module.mysql]
-#
-#  source                      = "./modules/app"
-#  ami_m                       = var.ami
-#  instance_type_m             = "t2.micro"
-#  vpc_security_group_ids_m    = var.vpc_security_group_ids
-#  tags_m                      = var.tags["backend"]
-#  component_m                 = "backend"
-#  env_m                       = var.env
-#}
+module "backend" {
+  depends_on = [module.mysql]
+
+  source                      = "./modules/app"
+  ami_m                       = var.ami
+  instance_type_m             = "t2.micro"
+  vpc_security_group_ids_m    = var.vpc_security_group_ids
+  tags_m                      = var.tags["backend"]
+  component_m                 = "backend"
+  env_m                       = var.env
+}
 
 module "mysql" {
   source                      = "./modules/app"
