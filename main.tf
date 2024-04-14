af5340478c0e0061483146f95e5b8803fbd6542f
chaitu1 <chaitu143@gmail.com>
2024-04-14 11:16:26 +0530

    elk set up is done

--- main.tf
+++ main.tf
@@ -49,17 +49,18 @@ provider "vault" {
 #  env_m                       = var.env
 #}
 ##
-#module "backend" {
-#  depends_on = [module.mysql]
-#
-#  source                      = "./modules/app"
-#  ami_m                       = "ami-090252cbe067a9e58"
-#  instance_type_m             = "t2.micro"
-#  vpc_security_group_ids_m    = var.vpc_security_group_ids
-#  tags_m                      = var.tags["backend"]
-#  component_m                 = "backend"
-#  env_m                       = var.env
-#}
+module "backend" {
+  depends_on = [module.mysql]
+
+  source                      = "./modules/app"
+  ami_m                       = "ami-090252cbe067a9e58"
+  instance_type_m             = "t2.micro"
+  vpc_security_group_ids_m    = var.vpc_security_group_ids
+  tags_m                      = var.tags["backend"]
+  component_m                 = "backend"
+  env_m                       = var.env
+  vault_token_m               = var.vault_token
+}
 
 module "mysql" {
   source                      = "./modules/app"
@@ -69,7 +70,7 @@ module "mysql" {
   tags_m                      = var.tags["mysql"]
   component_m                 = "mysql"
   env_m                       = var.env
-  vault_token_m                 = var.vault_token
+  vault_token_m               = var.vault_token
 }
 
 
