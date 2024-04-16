4cf1a53510c72a14ac08f170b9dd9e8ca0d7e283
chaitu1 <chaitu143@gmail.com>
2024-04-16 14:24:19 +0530

    vpc peering connection

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -6,7 +6,6 @@ resource "aws_vpc" "dev-vpc" {
   }
 }
 
-
 resource "aws_subnet" "dev-subnet" {
   vpc_id                  = aws_vpc.dev-vpc.id
   cidr_block              = var.subnet_cidr_block_m
@@ -17,7 +16,6 @@ resource "aws_subnet" "dev-subnet" {
   }
 }
 
-
 resource "aws_vpc_peering_connection" "foo" {
   peer_vpc_id   = var.default_vpc_id_m
   vpc_id        = aws_vpc.dev-vpc.id
@@ -26,4 +24,16 @@ resource "aws_vpc_peering_connection" "foo" {
   tags = {
     Name = "VPC Peering between ${var.env_m}_vpc to default_vpc"
   }
+}
+
+resource "aws_route" "dev-vpc-route" {
+  route_table_id              = aws_vpc.dev-vpc.default_route_table_id
+  vpc_peering_connection_id   = aws_vpc_peering_connection.foo.id
+  destination_cidr_block      = "172.31.0.0/16"
+}
+
+resource "aws_route" "default-vpc-route" {
+  route_table_id            = "rtb-0d3e433084e76a929"
+  vpc_peering_connection_id = aws_vpc_peering_connection.foo.id
+  destination_cidr_block    = aws_vpc.dev-vpc.cidr_block
 }
