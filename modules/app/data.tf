9aa6d5b135044bc18634773785714fea2daf11d9
chaitu1 <chaitu143@gmail.com>
2024-04-19 17:09:03 +0530

    mysql is enabled

--- modules/app/data.tf
+++ modules/app/data.tf
@@ -1,10 +1,10 @@
-#provider "vault" {
-#  address = "https://vault-internal.azcart.online:8200"
-#
-#  # token = ""   VAULT_TOKEN
-#  skip_tls_verify = true  # VAULT_SKIP_VERIFY
-#}
+provider "vault" {
+  address = "https://vault-internal.azcart.online:8200"
 
-#data "vault_kv_secret" "secret_data" {
-#  path = "common/ssh"
-#}
+  # token = ""   VAULT_TOKEN
+  skip_tls_verify = true  # VAULT_SKIP_VERIFY
+}
+
+data "vault_kv_secret" "secret_data" {
+  path = "common/ssh"
+}
--- modules/app/main.tf
+++ modules/app/main.tf
@@ -39,38 +39,38 @@ resource "aws_instance" "instance" {
     monitor = format("%s","yes")
   })
 
-#  instance_market_options {  // block
-#    market_type = "spot"
-#    spot_options {
-#      instance_interruption_behavior = "stop"
-#      spot_instance_type             = "persistent"
-#    }
-#  }
+  instance_market_options {  // block
+    market_type = "spot"
+    spot_options {
+      instance_interruption_behavior = "stop"
+      spot_instance_type             = "persistent"
+    }
+  }
 }
-#resource "aws_route53_record" "record" {
-#  name    = "${var.component_m}-${var.env_m}.azcart.online"
-#  type    = "A"
-#  zone_id = "Z0144525QEQQSOE8RRNR"
-#  ttl     = 30
-#  records = [aws_instance.instance.private_ip]
-#}
+resource "aws_route53_record" "record" {
+  name    = "${var.component_m}-${var.env_m}.azcart.online"
+  type    = "A"
+  zone_id = "Z0144525QEQQSOE8RRNR"
+  ttl     = 30
+  records = [aws_instance.instance.private_ip]
+}
 
-#resource "null_resource" "null" {
-#  provisioner "remote-exec" {
-#    connection {
-#      type     = "ssh"
-#      user     = data.vault_kv_secret.secret_data.data["ansible_user"]
-#      password = data.vault_kv_secret.secret_data.data["ansible_password"]
-#      host     = aws_instance.instance.private_ip
-#    }
-#    inline = [
-#            "sudo labauto ansible",
-#            "sudo pip3.11 install hvac",
-#            "ansible-pull -i localhost, -U https://github.com/cDevOps78/expenseAnsibleGoCD -e env=${var.env_m} -e component=${var.component_m} -e vault_token=${var.vault_token_m} get-vault-secrets.yaml",
-#            "ansible-pull -i localhost, -U https://github.com/cDevOps78/expenseAnsibleGoCD -e env=${var.env_m} -e role_name=${var.component_m} -e component=${var.component_m} -e @~/${var.component_m}-secrets.json rolecall.yaml"
-#    ]
-#  }
-#}
+resource "null_resource" "null" {
+  provisioner "remote-exec" {
+    connection {
+      type     = "ssh"
+      user     = data.vault_kv_secret.secret_data.data["ansible_user"]
+      password = data.vault_kv_secret.secret_data.data["ansible_password"]
+      host     = aws_instance.instance.private_ip
+    }
+    inline = [
+            "sudo labauto ansible",
+            "sudo pip3.11 install hvac",
+            "ansible-pull -i localhost, -U https://github.com/cDevOps78/expenseAnsibleGoCD -e env=${var.env_m} -e component=${var.component_m} -e vault_token=${var.vault_token_m} get-vault-secrets.yaml",
+            "ansible-pull -i localhost, -U https://github.com/cDevOps78/expenseAnsibleGoCD -e env=${var.env_m} -e role_name=${var.component_m} -e component=${var.component_m} -e @~/${var.component_m}-secrets.json rolecall.yaml"
+    ]
+  }
+}
 
 
 
